9aa970a20ce59c46541d3b5a46c5852c189f6b4f
andrewbangg <80579399+andrewbangg@users.noreply.github.com>
2025-04-15 06:37:23 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@
 DATABASE = {
     'default': {
         'ENGINE': '',
-        'NAME': 'general_wb_cell',
-        'USER': 'client_wb',
-        'PASSWORD': 'Samby595!',
-        'HOST': '130.193.59.240',
-        'PORT': '5432'
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
